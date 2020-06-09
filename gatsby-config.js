7e387e473f5bd3a026f6eb8820906d192c90df50
carnalito80 <kunbg_jk@hotmail.com>
2020-06-09 14:27:22 +0200

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Köksrenovering Stockholm',
+    siteUrl: 'https://www.xn--kksrenovering-stockholm-7kc.nu'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -12,7 +12,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         /*id: 'GTM-add_your_tag_here',*/
-        id: 'GTM-P4RNF8D',
+        id: '',
         includeInDevelopment: false
       }
     },
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'yellowcake',
-        short_name: 'yellowcake',
+        name: 'Köksrenovering Stockholm',
+        short_name: 'kök_stockholm',
         start_url: '/',
         background_color: '#00C2BD',
         theme_color: '#00C2BD',
