41ec0336092457275604c094b2a57983b5b48486
carnalito80 <kunbg_jk@hotmail.com>
2020-08-07 01:24:41 +0200

    lang added

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
+    {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'se'
+      }
+    },
     // {
     //   resolve: 'gatsby-plugin-google-tagmanager',
     //   options: {
