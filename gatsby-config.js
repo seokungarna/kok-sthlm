ac7bfd3badadc41e67b739f5e0ace207d4f55d07
carnalito80 <kunbg_jk@hotmail.com>
2021-06-24 12:12:45 +0200

    ny tid ny strid3

--- gatsby-config.js
+++ gatsby-config.js
@@ -132,14 +132,14 @@ module.exports = {
       }
     },
     'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-        stylesPath: `${__dirname}/src/cms/admin.css`,
-        enableIdentityWidget: true
-      }
-    },
+    // {
+    //   resolve: 'gatsby-plugin-netlify-cms',
+    //   options: {
+    //     modulePath: `${__dirname}/src/cms/cms.js`,
+    //     stylesPath: `${__dirname}/src/cms/admin.css`,
+    //     enableIdentityWidget: true
+    //   }
+    // },
      'gatsby-plugin-netlify' // make sure to keep it last in the array
   ]
 }
